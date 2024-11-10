4e9256dbc9f31fea125877888842c408394a860e
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2024-11-10 21:50:36 +0700

    create api for register

--- backend/api.js
+++ backend/api.js
@@ -10,12 +10,14 @@ const router = express.Router();
 
 // IMPORT CONTROLLERS
 import { Wellcome } from "./src/wellcome/wellcome.controller.js";
+import { Register } from "./src/auth/auth.controller.js";
 import { NotFound } from "./src/not_found/not_found.controller.js";
 
 
 
 // ROUTES
 router.get("/", Wellcome);
+router.post("/register", Register);
 router.get("/*", NotFound);
 
 
